89fd3493d34a6617d739c4567db45ffd3260b7a4
Patricia Orty <animdy@email.com>
2013-10-21 19:36:19 -0400

    Screencast - Do While Loop follow along

---
+++ WPF_Week04_TAKE2/Activity_DoWhileLoops/js/script.js
@@ -0,0 +1,13 @@
+// Screencast : Do While Loops
+
+//console.log('----------LOOPS----------');
+
+var c = 10;
+
+do{
+	console.log(c + 'Kegs on the Wall');
+	c--;
+}
+while(c > 0);
+
+//the do code is run prior to the condition so it will run prior to being seen as true or false.
