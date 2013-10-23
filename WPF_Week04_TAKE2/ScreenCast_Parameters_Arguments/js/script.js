6f7d3dbec193502d4a496870f3f0369d346f3cbc
Patricia Orty <animdy@email.com>
2013-10-22 22:41:36 -0400

    Addition of notes for follow along

--- WPF_Week04_TAKE2/ScreenCast_Parameters_Arguments/js/script.js
+++ WPF_Week04_TAKE2/ScreenCast_Parameters_Arguments/js/script.js
@@ -1,2 +1,21 @@
 // Screencast : Parameters and Arguments
 
+
+
+calcArea(30, 20);
+
+//w=30, h=20 
+
+function calcArea(w, h){
+	var area = w * h;
+	console.log(area);
+}
+
+
+function dogYears(age){  //parameters (storage container)
+	var dogYears = age * 7;
+	console.log("Sparky is " + dogYears + " years old.");
+}
+dogYears(10); //arguements
+var age1 = 4;  // can use arguements to pass variable information to parameters
+dogYears(age1);
