35167553908f727f38672c5e6135d9410d096cb1
Patricia Orty <animdy@email.com>
2013-10-23 20:12:17 -0400

    Screencast - Returning Values follow along

---
+++ WPF_Week04_TAKE2/ScreenCast_ReturningValues/js/script.js
@@ -0,0 +1,12 @@
+// Screencast : Returning Values
+
+
+
+var total = calcArea(30, 20); //( , ) are arguements
+
+
+function calcArea(w, h){  //( , ) Parameters 
+	var area = w * h;
+	return area; //function is spitting the information out... and this happens where the function is envoked
+}
+console.log(total);
