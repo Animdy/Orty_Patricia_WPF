0015004ac3137a3a0d0d538145c440bbe9fc4b89
Patricia Orty <animdy@email.com>
2013-10-25 22:32:43 -0400

    Setup of alert

--- WPF_Week04_TAKE2/Assignment_Functions/Functions_Wacky/js/script.js
+++ WPF_Week04_TAKE2/Assignment_Functions/Functions_Wacky/js/script.js
@@ -13,3 +13,5 @@ var dragonNail;
 var nameWish;
 
 //Alert to describe purpose of following prompts to gather information for calculation.
+alert("Here at Wonda's Web of the Wide World Potion Imporium we've designed away to mix your potions without a coldron and send the results through the wired. No fuss, no muss!\nNo more cleaning up from the boiling and no more toiling with finding ingredients.\nJust tell me how much of the next three ingredients and we'll tell you if it worked or etc.\nHappy spell-binding to our neo-webcasters!");
+
