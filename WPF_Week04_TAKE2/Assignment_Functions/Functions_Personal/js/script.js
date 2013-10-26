36d76f58a877799c189f92eb9625e4586ab2720d
Patricia Orty <animdy@email.com>
2013-10-25 22:54:01 -0400

    Fix of misspelling in Explanation of declaration of howManyRotations and addition of information to last definition explanation

--- WPF_Week04_TAKE2/Assignment_Functions/Functions_Personal/js/script.js
+++ WPF_Week04_TAKE2/Assignment_Functions/Functions_Personal/js/script.js
@@ -6,7 +6,7 @@
 
 //Goal: To determing outcome of coin toss set winner.
 
-//Declaration of variable howManyRoations
+//Declaration of variable howManyRotations
 var howManyRotations;
 
 //Declaration of variable howManyTimes
@@ -15,4 +15,4 @@ var howManyTimes
 //Alert to explain prompts to follow to gather information for calculation
 alert("Having trouble solving a problem. Well just answer the next two questions and we'll determine if you get heads or tails overall in your deciding coin flip.");
 
-//Definition of variable howManyRotations
+//Definition of variable howManyRotations with prompt
