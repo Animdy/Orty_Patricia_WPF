b38f5922d70cde21ca98a0d32b054c3b5eaca29b
Patricia Orty <animdy@email.com>
2013-10-25 22:53:10 -0400

    Explanation of declaration of variable howManyRoations with prompt

--- WPF_Week04_TAKE2/Assignment_Functions/Functions_Personal/js/script.js
+++ WPF_Week04_TAKE2/Assignment_Functions/Functions_Personal/js/script.js
@@ -14,3 +14,5 @@ var howManyTimes
 
 //Alert to explain prompts to follow to gather information for calculation
 alert("Having trouble solving a problem. Well just answer the next two questions and we'll determine if you get heads or tails overall in your deciding coin flip.");
+
+//Definition of variable howManyRotations
