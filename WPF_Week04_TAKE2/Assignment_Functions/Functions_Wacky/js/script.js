504c18c09c048ddfadae83bb94d54753d948b5fb
Patricia Orty <animdy@email.com>
2013-10-25 22:44:10 -0400

    Setup of console.log to show function result

--- WPF_Week04_TAKE2/Assignment_Functions/Functions_Wacky/js/script.js
+++ WPF_Week04_TAKE2/Assignment_Functions/Functions_Wacky/js/script.js
@@ -38,4 +38,4 @@ var potion = function(amount, ingredient, other, potionName){
 var result = potion(toads, root, dragonNail, nameWish);
 
 //console.log to show results of function
-
+console.log(result);
